637b6bdb77dc610626e9dd09a78d68468d7a2bf7
SVK_CU_NLP <96056131+svk-cu-nlp@users.noreply.github.com>
2023-03-27 16:22:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,23 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from transformers import pipeline
+import openai
+from PIL import Image
+openai.organization = "org-gjGFcDVcuXwEmPd8d451lKaK"
+openai.api_key = openai_key
+#openai.Model.list()
+pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
 
-"""
-# Welcome to Streamlit!
+st.title("Hot Dog? Or Not?")
+st.write(openai.Model.list())
+file_name = st.file_uploader("Upload a hot dog candidate image")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+if file_name is not None:
+    col1, col2 = st.columns(2)
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    image = Image.open(file_name)
+    col1.image(image, use_column_width=True)
+    predictions = pipeline(image)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    col2.header("Probabilities")
+    for p in predictions:
+        col2.subheader(f"{ p['label'] }: { round(p['score'] * 100, 1)}%")
